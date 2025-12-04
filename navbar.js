df1d823a4e1f2ffd29315ee7132a71d166e45cd9
ITClass <ITClassSL@gmail.com>
2025-12-04 16:29:47 +0530

    ss1

--- navbar.js
+++ navbar.js
@@ -28,13 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
                             </div>
                         </div>
                         <div class="menu-item">
-                            British Curriculum
+                            British Curriculum >
                             <div class="sub-menu">
                                 <a href="#">Grade 1</a>
                                 <a href="#">Grade 2</a>
-                                <a href="Grade-3-BC.html">Grade 3</a>
+                                <a href="Grade-3-BC.html">Grade 3 ></a>
                                 <a href="#">Grade 4</a>
-                                <a href="Grade-5-BC.html">Grade 5</a>
+                                <a href="Grade-5-BC.html">Grade 5 ></a>
                                 <a href="#">Grade 6</a>
                                 <a href="#">Grade 7</a>
                                 <a href="#">Grade 8</a>
